9f95c6b828387b85f2aba56c554d0673ff3cf379
fenglong <fenglong@xiaomi.com>
2014-07-29 11:45:01 +0800

    :camel:init

--- README.md
+++ README.md
@@ -2,3 +2,48 @@ ttf2svg
 =======
 
 Font convertor, TTF to SVG, for node.js
+
+
+Usage
+-----
+
+Install:
+
+``` bash
+npm install -g ttf2svg
+```
+
+Usage example:
+
+``` bash
+ttf2svg fontello.ttf fontello.svg
+```
+
+Or:
+
+``` bash
+ttf2svg < fontello.ttf > fontello.svg
+```
+
+Or:
+
+``` javascript
+var ttf2svg = require('ttf2svg')
+  , fs = require('fs')
+;
+
+fs.readFile('./fontello.ttf', function (err, buffer) {
+    if (!!err) throw err;
+
+    var svgContent = ttf2svg(buffer);
+    fs.writeFileSync('./fontello.svg', svgContent);
+
+});
+
+```
+
+
+Reference
+-----
+
+[gulp-ttf2svg](https://github.com/morlay/gulp-ttf2svg/)
---
+++ bin/ttf2svg.js
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+
+/*
+
+ttf2svg ./FZLTCXHJW.ttf ./
+
+*/
+
+var ttf2svg = require('../src/index')
+  , fs = require('fs')
+  , path = require('path')
+;
+
+var fontPath = process.argv[2];
+var fontOutputPath = process.argv[3] || './';
+
+var basename = path.basename(fontPath, path.extname(fontPath));
+var fontOutputFilePath = path.join(fontOutputPath, basename + '.svg');
+
+fs.readFile(fontPath, function (err, buffer) {
+    if (!!err) throw err;
+
+    // return string
+    var svgContent = ttf2svg(buffer);
+    fs.writeFileSync(fontOutputFilePath, svgContent);
+
+    console.log('complete : '+fontOutputFilePath+'!');
+});
